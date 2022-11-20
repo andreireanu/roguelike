1fa6048c91ba3b58d63fd2c0c85df5a50ec5ae63
andrei <andrei.reanu@gmail.com>
2022-11-20 20:48:24 +0200

    Add rooms

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use bracket_lib::prelude::*;
 use std::fmt::Display;
 use specs::prelude::*;
 
+mod map;
 mod components;
 use crate::components::*;  
-
+use crate::map::*;
 
 embedded_resource!(WIDE_FONT, "resources/terminal_10x16.png");
 embedded_resource!(VGA_FONT, "resources/vga8x16.png");
 
-const WIDTH: i32 = 40;
+const WIDTH:  i32 = 40;
 const HEIGHT: i32 = 25;
 pub enum RunState{
     WelcomeScreen,
@@ -39,11 +40,9 @@ impl GameState for State {
                 }  
             }
             RunState::StartGame => {
-                let mut r = self.ecs.get_mut::<RandomNumberGenerator>().unwrap().range(10,100);
-                self.center_at_row(ctx, 12, "Game has begun");
-                self.center_at_row(ctx, 15, r);
                 ctx.print(0, 0, format!("{}", ctx.fps));
-
+                let map = self.ecs.fetch::<Map>();
+                map.draw(ctx);
             }
         }
     }
@@ -57,7 +56,7 @@ impl State {
     } 
 }
 
-
+ 
 fn main() -> BError { 
      link_resource!(WIDE_FONT, "resources/terminal_10x16.png");
      link_resource!(VGA_FONT, "resources/vga8x16.png");
@@ -67,7 +66,7 @@ fn main() -> BError {
         .with_title("Roguelike Tutorial")
         .with_font("terminal_10x16.png", 10, 16)
         .with_tile_dimensions(10, 16)
-        .with_fps_cap(10.0)
+        .with_fps_cap(10.0)      
         .build()?;
 
     let mut gs = State{ 
@@ -75,7 +74,9 @@ fn main() -> BError {
         display: RunState::WelcomeScreen};
 
     register_components(&mut gs.ecs);
-    gs.ecs.insert(RandomNumberGenerator::new());  
+    gs.ecs.insert(RandomNumberGenerator::new()); 
+    let map = Map::generate(&mut gs.ecs, WIDTH, HEIGHT);
+    gs.ecs.insert(map); 
 
     rltk::main_loop(context, gs)
 } 
---
+++ src/map.rs
@@ -0,0 +1,43 @@
+use specs::prelude::*; 
+use bracket_lib::prelude::*; 
+
+pub struct Map{
+    rooms: Vec<Rect>,
+} 
+
+const MIN_WIDTH: i32 = 2;
+const MAX_WIDTH: i32 = 9;
+const MIN_HEIGHT: i32 = 2;
+const MAX_HEIGHT: i32 = 7;
+
+impl Map {
+    pub fn generate (ecs: &mut World, width : i32, height: i32) -> Self {
+        let mut rooms = Vec::new();
+        let rng = ecs.get_mut::<RandomNumberGenerator>().unwrap();
+        while rooms.len() < 5 {
+              let x1 = rng.range(0, width - MAX_WIDTH);
+              let y1 = rng.range(0, height - MAX_HEIGHT );
+              let w = MIN_WIDTH + rng.range(0, MAX_WIDTH - MIN_WIDTH);
+              let h = MIN_HEIGHT  + rng.range(0, MAX_HEIGHT - MIN_HEIGHT);
+              let new_room = Rect::with_size(x1, y1, w, h);
+              let mut intersect = false;
+              for r in &rooms {
+                if new_room.intersect(&r){
+                    intersect = true;
+                };
+              }
+              rooms.push(new_room);
+        }  
+        Self { rooms}
+    }
+
+    pub fn draw(&self, ctx: &mut BTerm) {
+        for room in &self.rooms {
+            for col in room.x1..room.x2 {
+                for row in room.y1..room.y2 {
+                    ctx.print(col, row, '#');
+                }
+            }
+        }
+    }
+}
